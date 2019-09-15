3005d712cdaf1b3691d291025ae15808dfcd546e
joraavanes <jora_a@outlook.com>
2019-09-15 21:44:02 +0430

    Start command modified for Heroku

--- app/middleware/printIp.js
+++ app/middleware/printIp.js
@@ -1,5 +1,6 @@
 
 module.exports = (req, res, next) => {
-    console.log(req.connection.remoteAddress || req.headers['x-forwarded-for']);
+    console.log(req.headers['x-forwarded-for']);
+    // console.log(req.connection.remoteAddress || req.headers['x-forwarded-for']);
     next();
 };
