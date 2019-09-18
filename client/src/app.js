42f06a5abe398e0a7586a179f1ccfc8ed2295e5d
joraavanes <jora_a@outlook.com>
2019-09-18 17:29:26 +0430

    Router added - fixes needed

--- client/src/app.js
+++ client/src/app.js
@@ -1,70 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Container, Button, Row, Col} from 'reactstrap'
+import { BrowserRouter as Router,Switch, Route, Link } from 'react-router-dom'
+import App from './components/app'
 import Navigation from './components/Navigation'
-import SearchProduct from './components/SearchProduct'
-import Items from './components/Items'
+import {products} from './mock/mock'
+import 'bootstrap'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/custom.scss'
 
-import {products} from './mock/mock'
+const router = (
+    <Router>
+        <Navigation/>
+        <Switch>
+            <Route path="/" Component={() => <App products={products}/>}/>
+        </Switch>
+    </Router>
+);
 
-// import img from './assets/mac.jpg'
-
-class App extends React.Component{
-    state = {
-        pageTitle: 'Welcome to Barkley\'s Store',
-        filteredItems: this.props.products
-    };
-
-    handleHeader = headerTitle => {
-        // const target = e.target;
-        
-        if(headerTitle !== ''){
-            const filteredItems = this.props.products.filter(item => item.name.includes(headerTitle));
-            this.setState(() => ({ filteredItems }));
-        }else{
-            this.setState(() => ({ filteredItems: this.props.products }));
-        }
-
-        headerTitle  = headerTitle !== '' ? headerTitle: 'Welcome to Barkley\'s Store';
-        this.setState(() => ({pageTitle: headerTitle}));
-    }
-
-    render(){
-        return(
-            <React.Fragment>
-                <Navigation/>
-                <SearchProduct handleHeader={this.handleHeader}/>
-                <Container>
-                    <Row>
-                        {/*
-                        <Col>
-                             <form onSubmit={this.handleFormSubmit}>
-                                <div className="form=group">
-                                    <input type="text" className="form-control" id="message" name="message" onChange={this.handleHeader} onCopy={this.handleCopyText}/>
-                                </div>
-                                <p>
-                                    <Button type="submit">Send</Button>
-                                </p>
-                            </form> 
-                        </Col>*/}
-                        <Col>
-                            <h2 className="text-center">{this.state.pageTitle}</h2>
-                            {/*<input type="hidden" name="year" id="year" value={new Date().valueOf()}/> */}
-                            {/* <div style={{display: 'flex', flexDirection: 'column', alignItems:'center'}}>
-                                <Button color="success" onClick={this.handleWelcome}>click on me</Button>
-                            </div> */}
-                            {/* <img src={img}/> */}
-                        </Col>
-                    </Row>
-                </Container>
-                <Items products={this.state.filteredItems}/>
-            </React.Fragment>
-        );
-    }
-}
-
-
-
-ReactDOM.render(<App products={products}/>, document.querySelector('#app'));
+ReactDOM(router, document.querySelector('#app'));
---
+++ client/src/components/app.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Container, Button, Row, Col} from 'reactstrap'
+import SearchProduct from './SearchProduct'
+import Items from './Items'
+
+// import {products} from './mock/mock'
+
+// import img from './assets/mac.jpg'
+
+class App extends React.Component{
+    state = {
+        pageTitle: 'Welcome to Barkley\'s Store',
+        filteredItems: this.props.products
+    };
+
+    handleHeader = headerTitle => {
+        // const target = e.target;
+        
+        if(headerTitle !== ''){
+            const filteredItems = this.props.products.filter(item => item.name.includes(headerTitle));
+            this.setState(() => ({ filteredItems }));
+        }else{
+            this.setState(() => ({ filteredItems: this.props.products }));
+        }
+
+        headerTitle  = headerTitle !== '' ? headerTitle: 'Welcome to Barkley\'s Store';
+        this.setState(() => ({pageTitle: headerTitle}));
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                
+                <SearchProduct handleHeader={this.handleHeader}/>
+                <Container>
+                    <Row>
+                        <Col>
+                            <h2 className="text-center">{this.state.pageTitle}</h2>
+                            {/*<input type="hidden" name="year" id="year" value={new Date().valueOf()}/> */}                            
+                            {/* <img src={img}/> */}
+                        </Col>
+                    </Row>
+                </Container>
+                <Items products={this.state.filteredItems}/>
+            </React.Fragment>
+        );
+    }
+}
+
+
+
+// ReactDOM.render(<App products={products}/>, document.querySelector('#app'));
+export default App;
