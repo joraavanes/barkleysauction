5bfde8befc1e4bee8d862990a4b10e225fae2ddf
joraavanes <jora_a@outlook.com>
2019-10-10 13:22:45 +0330

    Mock changed

--- client/src/mock/mock.js
+++ client/src/mock/mock.js
@@ -23,6 +23,11 @@ const products = [
     {name: 'bicycle',price: 5410, sold:false, img:'bag.jpg'},
     {name: 'toaster',price: 112, sold:false, img:'bag.jpg'},
     {name: 'shover',price: 112, sold:false, img:'bag.jpg'},
+    {name: 'baggage',price: 112, sold:false, img:'bag.jpg'},
+    {name: 'Handbag',price: 1120, sold:false, img:'bag.jpg'},
+    {name: 'bicycle',price: 5410, sold:false, img:'bag.jpg'},
+    {name: 'toaster',price: 112, sold:false, img:'bag.jpg'},
+    {name: 'shover',price: 112, sold:false, img:'bag.jpg'},
     {name: 'baggage',price: 112, sold:false, img:'bag.jpg'}
 ];
 
