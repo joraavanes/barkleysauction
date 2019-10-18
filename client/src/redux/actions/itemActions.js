c7201d39b1102ff0ae843e41629b0a9d9957baf1
joraavanes <jora_a@outlook.com>
2019-10-18 16:03:58 +0330

    Built for remote repos - fixes

--- client/src/redux/actions/itemActions.js
+++ client/src/redux/actions/itemActions.js
@@ -1,9 +1,11 @@
 import {ADD_ITEM,EDIT_ITEM,GET_ITEM,GET_ITEMS,ITEMS_LOADING,CLEAR_ITEMS} from './types/types'
 import axios from 'axios'
 
+const url = process.env.NODE_ENV === 'production'? 'https://protected-scrubland-62320.herokuapp.com/': 'http://localhost:3000'
+
 export const getItems = () => dispatch => {
     dispatch(itemsLoading());
-    axios.get('http://localhost:3000/items')
+    axios.get(`${url}/items`)
         .then(res=>{
             dispatch({
                 type:GET_ITEMS,
@@ -15,12 +17,12 @@ export const getItems = () => dispatch => {
 
 export const getItem = (name,id) => dispatch => {
     dispatch(itemsLoading());
-    axios.get(`http://localhost:3000/items/${name}/${id}`)
+    axios.get(`${url}/items/${name}/${id}`)
         .then(res => {
             dispatch({
                 type: GET_ITEM,
                 loading: false,
-                items: res.data    
+                items: res.data
             });
         });
 };
