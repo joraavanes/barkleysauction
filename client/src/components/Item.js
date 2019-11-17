74b1c5f6df155f301926b5876299567e2a44e969
joraavanes <jora_a@outlook.com>
2019-11-18 01:37:28 +0330

    item image added - scss variable

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {NavLink} from 'react-router-dom'
+import bag from '../media/backpack.jpg'
 
 const Item = (props) => {
     return (
@@ -8,6 +9,7 @@ const Item = (props) => {
         // </div>
         <div className="col-12 col-sm-6 col-md-3 col-lg-2">
             <div className="card">
+                <img src={bag} className="img-fluid"/>
                 <div className="card-body">
                     <h5 className="card-title">{props.name}</h5>
                     {/* <p className="card-text">With supporting text below as a natural lead-in to additional content.</p> */}
