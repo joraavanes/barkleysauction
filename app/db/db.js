8b727a766629149777ae49af49584bd2595c0635
joraavanes <jora_a@outlook.com>
2019-12-22 13:51:52 +0330

    heroku publish - fix

--- app/db/db.js
+++ app/db/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+console.log(process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://barkleys:fn3FP!UW9JfqVe5@ds257698.mlab.com:57698/heroku_jlhrjp4x',{
     useNewUrlParser: true,
     useFindAndModify: false,
@@ -8,6 +9,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://barkleys:fn3FP!UW9JfqVe5@
     useUnifiedTopology: true
 })
 .then(() => console.log('Connected to database successfully'))
-.catch(() => console.log('Failed to connect to database'));
+.catch((err) => console.log('Failed to connect to database',JSON.stringify(err, undefined, 3)));
 
 module.exports = {mongoose};
