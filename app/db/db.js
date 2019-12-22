bcebff6e4fdfc2bd341b899b9d777b373bffb975
joraavanes <jora_a@outlook.com>
2019-12-22 14:33:03 +0330

    heroku publish - fix 3

--- app/db/db.js
+++ app/db/db.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-console.log(process.env.MONGODB_URI);
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://barkleys:fn3FP!UW9JfqVe5@ds257698.mlab.com:57698/heroku_jlhrjp4x',{
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jora:OjoEHwrKqsLhimdo@cluster0-8jnyu.mongodb.net/barkleys?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://jora:OjoEHwrKqsLhimdo@cluster0-8jnyu.mongodb.net/barkleys?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
@@ -11,5 +10,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jora:OjoEHwrKqsLhimdo
 })
 .then(() => console.log('Connected to database successfully'))
 .catch((err) => console.log('Failed to connect to database',JSON.stringify(err, undefined, 3)));
-
+    
 module.exports = {mongoose};
