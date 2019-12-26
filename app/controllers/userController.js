95a8203392b0aef7f0e2bbade2c004c9cd6ed9b3
joraavanes <jora_a@outlook.com>
2019-12-26 17:53:50 +0330

    userController added

---
+++ app/controllers/userController.js
@@ -0,0 +1,36 @@
+const User = require('../models/User');
+
+// POST: /users/register
+exports.register = (req, res) => {
+    const {name, surname, email, password } = req.body;
+    let user = new User({name, surname, email, password});
+
+    user.register()
+        .then(user => user.generateAuthToken())
+        .then(user => res.header('x-auth',user.token).send(user))
+        .catch(err => res.status(403).send({errors: err.errors}));
+};
+
+// POST: /users/login
+exports.login = (req, res) => {
+    const {email, password} = req.body;
+    const user = new User({email, password});
+
+    user.login()
+        .then(user=> user.generateAuthToken())
+        .then(user => res.header('x-auth', user.token).send(user))
+        .catch(err => res.status(401).send({errors: err}));
+};
+
+// POST: /users/logout
+exports.logout =  (req,res) => {
+    const {token} = req.body;
+    User.removeToken(token)
+        .then(user => res.send({msg: 'Logged out successfully', user}))
+        .catch(() => res.status(401).send('Invalid token'));
+};
+
+// GET: /users/verifyToken
+exports.verifyToken = (req, res) => {
+    res.send(`Hello dear ${req.user.name} ${req.user.surname}. You are verified!`);
+};
--- app/routes/Users.js
+++ app/routes/Users.js
@@ -1,39 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../models/User');
 const authenticate = require('../middleware/authenticate');
+const userController = require('../controllers/userController');
 
-router.post('/register', (req, res) =>{
-    const {name, surname, email, password } = req.body;
-    let user = new User({name, surname, email, password});
+router.post('/register', userController.register);
 
-    user.register()
-        .then(user => user.generateAuthToken())
-        .then(user => res.header('x-auth',user.token).send(user))
-        .catch(err => res.status(403).send({errors: err.errors}));
-});
+router.post('/login', userController.login);
 
-router.post('/login', (req, res) => {
-    const {email, password} = req.body;
-    const user = new User({email, password});
+router.post('/logout', authenticate, userController.logout);
 
-    user.login()
-        .then(user=> user.generateAuthToken())
-        .then(user => res.header('x-auth', user.token).send(user))
-        .catch(err => res.status(401).send({errors: err}));
-});
-
-router.post('/logout', authenticate, (req,res) => {
-    const {token} = req.body;
-    User.removeToken(token)
-        .then(user => res.send({msg: 'Logged out successfully', user}))
-        .catch(() => res.status(401).send('Invalid token'));
-});
-
-router.get('/verifyToken', authenticate, (req, res) => {
-
-    res.send(`Hello dear ${req.user.name} ${req.user.surname}. You are verified!`);
-});
+router.get('/verifyToken', authenticate, userController.verifyToken);
 
 module.exports = router;
