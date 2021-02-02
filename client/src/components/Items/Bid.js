035aa73bb6d86b9361142fd9281e3038d41230ec
joraavanes <jora_a@outlook.com>
2021-02-02 15:38:29 +0330

    vulnerable packages on server app updated

--- client/src/components/Items/Bid.js
+++ client/src/components/Items/Bid.js
@@ -12,10 +12,10 @@ class Bid extends Component {
             <>
                 <h2>Your Bid</h2>
                 <form onSubmit={this.handleBidSubmit} className={styles.BidForm}>
-                    <input type="number" step="0.01" className={styles.BidPrice} name="BidPrice" id="BidPrice"/>
+                    <input type="number" step="0.01" className={styles.BidPrice} name="BidPrice" id="BidPrice" placeholder="e.g. 29.99" autoComplete="off"/>
                     <button type="submit" value="Add Bid" className={styles.BidSubmitBtn} id="bid-submit-btn">
                         Add Bid
-                        {this.props.loading && <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>} 
+                        {/* <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> */}
                     </button>
                 </form>
                 
