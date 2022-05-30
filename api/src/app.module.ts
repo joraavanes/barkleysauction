a0a1c842d59550b5bfbe735ef904558756e8d142
Jora <jora_a@outlook.com>
2022-05-30 17:44:16 +0430

    Configure mongoose in app module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -7,8 +8,14 @@ import { BidsModule } from './bids/bids.module';
 import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, BidsModule, CommentsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/barkleysauction'),
+    UsersModule,
+    ProductsModule,
+    BidsModule,
+    CommentsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
