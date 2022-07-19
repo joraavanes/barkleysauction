643ecd70cf6190dded73811eff1d09e4099e7028
Jora <jora_a@outlook.com>
2022-07-19 18:02:38 +0430

    Add validationPipe to the global config

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true // Prevents from overposting
+    })
+  );
+  
   await app.listen(3000);
 }
 bootstrap();
