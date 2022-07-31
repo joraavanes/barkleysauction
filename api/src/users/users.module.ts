b12e16fda6f269b0c4dce5ddd5ba5ab0649d65e3
Jora <jora_a@outlook.com>
2022-07-31 09:27:44 +0430

    Add mongoose feature to Users module

--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -2,11 +2,30 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './user.schema';
+import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+    MongooseModule.forFeatureAsync([{
+      name: User.name,
+      // schema: UserSchema,
+      useFactory: () => {
+        const schema = UserSchema;
+        schema.pre('save', function () {
+          console.log('Hello from pre save');
+        });
+        schema.post('save', function () {
+          console.log('Hello from post save');
+        });
+        schema.post('updateOne', function () {
+          console.log('Some user is updated');
+        });
+        schema.post('deleteOne', function () {
+          console.log('Some user data deleted');
+        })
+        return schema;
+      }
+    }])
   ],
   providers: [UsersService],
   controllers: [UsersController]
