1575c557c223b304e47941b2a16eb670a05af66c
Jora <jora_a@outlook.com>
2022-08-14 13:55:03 +0430

    Refine SignInUserDto

--- api/src/users/dtos/signin-user.dto.ts
+++ api/src/users/dtos/signin-user.dto.ts
@@ -1,14 +1,12 @@
-import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from "class-validator";
 
 export class SignInUserDto {
-    @IsEmail({
-        message: 'Please enter your email'
-    })
-    Email: string;
+    @IsEmail({}, { message: 'The email address is not valid' })
+    @IsNotEmpty({message: 'Please enter your email address'})
+    email: string;
 
-    @IsString()
-    @IsNotEmpty({
-        message: 'Please enter your password'
-    })
-    Password: string;
+    @IsNotEmpty({ message: 'Please enter your password' })
+    @IsString({ message: 'Password must be of type string' })
+    @MaxLength(50, { message: "The password is at most 50 length of characters" })
+    password: string;
 }
