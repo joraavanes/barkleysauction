2e2f7d7a362f650443364bfa0783e3cd7680a3da
Jora <jora_a@outlook.com>
2022-10-03 14:43:20 +0330

    Remove nest app

--- api/src/bids/bids.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BidsService } from './bids.service';
-import { BidsController } from './bids.controller';
-
-@Module({
-  providers: [BidsService],
-  controllers: [BidsController]
-})
-export class BidsModule {}
--- api/src/products/products.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
-
-@Injectable()
-export class ProductsService {
-    constructor(
-        private usersSerivce: UsersService
-    ) { }
-}
