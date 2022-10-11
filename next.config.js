742a92a05facce6bbeae20e02ba230bc6a26c622
Jora <jora_a@outlook.com>
2022-10-11 11:52:30 +0330

    Add mongo credentials to env variables

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    MONGO_USER_ID: 'jora',
+    MONGO_USER_PASSWORD: 'emPvJbvkwZU8mdbg',
+    MONGO_DB_NAME: 'barkleys'
+  }
 }
 
 module.exports = nextConfig
--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -8,7 +8,7 @@ export class MongoDbClient {
     async getClient() {
         if (!this.mongoClient) {
             this.mongoClient = await MongoClient.connect(
-                'mongodb://localhost:27017/barkleysauction'
+                `mongodb+srv://${process.env.MONGO_USER_ID}:${process.env.MONGO_USER_PASSWORD}@cluster0-8jnyu.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`
             );
         }
         return this.mongoClient;
