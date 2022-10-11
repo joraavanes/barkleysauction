f334dc1820e94f6f55c16a91bcbab8655b013cdb
Jora <jora_a@outlook.com>
2022-10-11 13:25:57 +0330

    Add connection string generator for dev & prod envs

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -5,12 +5,20 @@ import { Service } from 'typedi';
 export class MongoDbClient {
     private mongoClient: MongoClient;
 
-    async getClient() {
+    async getClient(): Promise<MongoClient> {
+        const connString = this.getConnectionString();
         if (!this.mongoClient) {
-            this.mongoClient = await MongoClient.connect(
-                `mongodb+srv://${process.env.MONGO_USER_ID}:${process.env.MONGO_USER_PASSWORD}@cluster0-8jnyu.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`
-            );
+            this.mongoClient = await MongoClient.connect(connString);
         }
         return this.mongoClient;
+    }
+
+    getConnectionString(): string {
+        const isProd = process.env.NODE_ENV === 'production';
+
+        if (!isProd)
+            return `mongodb://localhost:27017/${process.env.MONGO_DB_NAME}`
+
+        return `mongodb+srv://${process.env.MONGO_USER_ID}:${process.env.MONGO_USER_PASSWORD}@cluster0-8jnyu.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`;
     }
 }
