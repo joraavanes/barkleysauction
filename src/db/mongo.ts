fcc1da00097191cd0ec9f4d05bdf7843ca2c9b7e
Jora <jora_a@outlook.com>
2022-10-21 15:32:16 +0330

    Fix connection string - Add connectionTimeout

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -7,8 +7,12 @@ export class MongoDbClient {
 
     async getClient(): Promise<MongoClient> {
         const connString = this.getConnectionString();
+        console.log(connString);
         if (!this.mongoClient) {
-            this.mongoClient = await MongoClient.connect(connString);
+            this.mongoClient = await MongoClient.connect(connString, {
+                ssl: true,
+                sslValidate: true
+            });
         }
         return this.mongoClient;
     }
@@ -19,6 +23,6 @@ export class MongoDbClient {
         if (!isProd)
             return `mongodb://localhost:27017/${process.env.MONGO_DB_NAME}`
 
-        return `mongodb+srv://${process.env.MONGO_USER_ID}:${process.env.MONGO_USER_PASSWORD}@cluster0-8jnyu.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`;
+        return `mongodb+srv://${process.env.MONGO_USER_ID}:${process.env.MONGO_USER_PASSWORD}@cluster0-8jnyu.mongodb.net/${process.env.MONGO_DB_NAME}?keepAlive=true&socketTimeoutMS=360000&connectTimeoutMS=360000&retryWrites=true&w=majority`;
     }
 }
