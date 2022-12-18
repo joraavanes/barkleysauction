7bc602762fd0ca5a10e81fc9d6c3b66ef112336e
Jora <jora_a@outlook.com>
2022-12-18 22:53:19 +0330

    Add context & provider to Bid component - Add useBid hook

--- components/item/Bid.tsx
+++ components/item/Bid.tsx
@@ -1,7 +1,38 @@
+import React, { createContext, useContext, useState } from "react";
+
 interface BidProps {
   children?: JSX.Element | JSX.Element[];
 }
 
+interface BidState {
+  status: string;
+  bids: number[];
+  newBid?: number;
+}
+
+interface BidContextType {
+  state: BidState;
+  setState(state: BidState): void;
+}
+
+const BidContext = createContext<BidContextType | null>(null);
+
 export const Bid: React.FC<BidProps> = ({ children }) => {
-  return <>{children}</>;
+  const [state, setState] = useState<BidState>({
+    bids: [53, 39, 23, 5, 91, 16, 6],
+    status: 'idle'
+  });
+
+  return (
+    <BidContext.Provider value={{ state, setState }}>
+      {children}
+    </BidContext.Provider>
+  );
+};
+
+export const useBid = () => {
+  const state = useContext(BidContext) as BidContextType;
+  if(!state)
+    throw new Error('Component must be within the BidContext.Provider');
+  return state;
 };
