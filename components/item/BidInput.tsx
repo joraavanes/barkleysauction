813a438bef65177bb569cbd5a40c16034e5d2818
Jora <jora_a@outlook.com>
2022-12-19 12:46:53 +0330

    Add context and implement bidding update

--- components/item/BidInput.tsx
+++ components/item/BidInput.tsx
@@ -1,10 +1,47 @@
+import { FormEvent, useState } from "react";
+import { useBid } from "./Bid";
 import { Button } from "./Button";
 
-export const BidInput: React.FC = () => (
-  <>
-    <form>
-      <input type="number" name="bid" id="bid" />
-      <Button state="success">Add bid</Button>
-    </form>
-  </>
-);
+const sleep = (timeout: number) =>
+  new Promise((resolve) => setTimeout(resolve, timeout));
+
+async function updateBid(newBid: number, lastBids: number[]) {
+  await sleep(1500);
+  if (newBid <= Math.max(...lastBids)) {
+    return Promise.reject({ message: "Bid must be higher than previous ones" });
+  }
+  return [newBid, ...lastBids].slice(0, 10);
+}
+
+export const BidInput: React.FC = () => {
+  const {
+    state: { newBid, bids, status },
+    setState,
+  } = useBid();
+  const [bid, setBid] = useState(0);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    setState({ status: "pending", newBid: bid, bids });
+    updateBid(bid, bids).then(
+      (state) => setState({ status: "success", bids: state }),
+      (error) => setState({ status: "fail", bids })
+    );
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          name="bid"
+          id="bid"
+          value={bid}
+          onChange={(e) => setBid(e.target.valueAsNumber)}
+        />
+        <Button state="success">Add bid</Button>
+      </form>
+    </>
+  );
+};
