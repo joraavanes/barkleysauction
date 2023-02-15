633736771c96a3edeae9656df7e4611bf65127c6
Jora <jora_a@outlook.com>
2023-02-15 12:33:00 +0330

    Print error message from the state

--- components/item/Comment/CommentStatus.tsx
+++ components/item/Comment/CommentStatus.tsx
@@ -2,10 +2,10 @@ import { Status, useComment } from "./Comment";
 
 export const CommentStatus: React.FC = () => {
   const [state] = useComment();
-  const { status } = state;
+  const { status, error } = state;
 
   if (status === Status.pending) return <>loading</>;
-  if (status === Status.error) return <div role="alert">Error</div>;
+  if (status === Status.error) return <div role="alert">{error}</div>;
 
   return <>{Status[status]}</>;
 };
