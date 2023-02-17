e09f41f08a9e36ed6722a996c30083937062940e
Jora <jora_a@outlook.com>
2023-02-17 17:12:00 +0330

    Add a test for displaying new comments on the list - Add wrapper for including context to tests

--- components/item/Comment/__tests__/CommentList.test.tsx
+++ components/item/Comment/__tests__/CommentList.test.tsx
@@ -1,15 +1,34 @@
+import userEvent from "@testing-library/user-event";
+import { faker } from "@faker-js/faker";
 import {
   render,
   screen,
   waitForElementToBeRemoved,
 } from "@testing-library/react";
-import { Comment, CommentStatus, CommentList } from "@/components/item/Comment";
+import {
+  Comment,
+  CommentStatus,
+  CommentList,
+  CommentForm,
+} from "@/components/item/Comment";
 
 const CommentListWithProvider = () => (
   <Comment>
     <CommentStatus />
     <CommentList />
   </Comment>
+);
+
+function Wrapper({ children } : {children: JSX.Element | JSX.Element[]}) {
+  return <Comment>{children}</Comment>;
+}
+
+const ComponentTree = (props) => (
+  <>
+    <CommentForm />
+    <CommentStatus />
+    <CommentList />
+  </>
 );
 
 test("CommentList should print 4 comments", async () => {
@@ -19,4 +38,26 @@ test("CommentList should print 4 comments", async () => {
 
   expect(commentList).toBeDefined();
   expect(commentList.length).toBe(4);
+});
+
+test("should display the posted comment in the CommentList", async () => {
+  render(<ComponentTree />, { wrapper: Wrapper });
+  await waitForElementToBeRemoved(() => screen.getByText(/loading/i), {
+    timeout: 2000,
+  });
+  
+  const submitBtn = screen.getByRole("button", { name: /post/i });
+  const commentInput = screen.getByRole('textbox');
+  
+  const commentText = faker.lorem.paragraph(1);
+  await userEvent.type(commentInput, commentText);
+  await userEvent.click(submitBtn);
+  
+  await waitForElementToBeRemoved(() => screen.getByText(/loading/i), {
+    timeout: 2000,
+  });
+
+  const commentItems = screen.getAllByRole('listitem');
+  expect(screen.getByText(commentText)).toBeInTheDocument();
+  expect(commentItems.length).toBe(5);
 });
