d252d3d294e6af725611945e67a5a520adc8c07b
Jora <jora_a@outlook.com>
2023-02-20 16:42:55 +0330

    Remove old wrapper for comments' tests

--- components/item/Comment/__tests__/CommentList.test.tsx
+++ components/item/Comment/__tests__/CommentList.test.tsx
@@ -12,14 +12,7 @@ import {
   CommentForm,
 } from "@/components/item/Comment";
 
-const CommentListWithProvider = () => (
-  <Comment>
-    <CommentStatus />
-    <CommentList />
-  </Comment>
-);
-
-function Wrapper({ children } : {children: JSX.Element | JSX.Element[]}) {
+function Wrapper({ children }: { children: JSX.Element | JSX.Element[] }) {
   return <Comment>{children}</Comment>;
 }
 
@@ -32,7 +25,7 @@ const ComponentTree = (props) => (
 );
 
 test("CommentList should print 4 comments", async () => {
-  render(<CommentListWithProvider />);
+  render(<ComponentTree />, { wrapper: Wrapper });
   await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
   const commentList = screen.getAllByRole("listitem");
 
@@ -45,19 +38,19 @@ test("should display the posted comment in the CommentList", async () => {
   await waitForElementToBeRemoved(() => screen.getByText(/loading/i), {
     timeout: 2000,
   });
-  
+
   const submitBtn = screen.getByRole("button", { name: /post/i });
-  const commentInput = screen.getByRole('textbox');
-  
+  const commentInput = screen.getByRole("textbox");
+
   const commentText = faker.lorem.paragraph(1);
   await userEvent.type(commentInput, commentText);
   await userEvent.click(submitBtn);
-  
+
   await waitForElementToBeRemoved(() => screen.getByText(/loading/i), {
     timeout: 2000,
   });
 
-  const commentItems = screen.getAllByRole('listitem');
+  const commentItems = screen.getAllByRole("listitem");
   expect(screen.getByText(commentText)).toBeInTheDocument();
   expect(commentItems.length).toBe(5);
 });
