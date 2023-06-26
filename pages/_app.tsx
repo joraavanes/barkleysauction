7f338161c5d37057dac069e4458115a4fd0030ef
Jora <jora_a@outlook.com>
2023-06-26 13:45:56 +0330

    Add react-query provider to app root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import "../styles/globals.css";
 import "reflect-metadata";
 import { NextPage } from "next";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
-import NextProgress from 'nextjs-progressbar';
+import NextProgress from "nextjs-progressbar";
 import { ReactElement, ReactNode } from "react";
 import MainLayout from "../components/layout/Layout";
 import { ErrorBoundary } from "../components/shared/";
@@ -15,14 +16,18 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout =
     Component.getLayout ?? ((page) => <MainLayout>{page}</MainLayout>);
 
   return getLayout(
     <ErrorBoundary>
-      <NextProgress color="#F91890"/>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <NextProgress color="#F91890" />
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </ErrorBoundary>
   );
 }
