1a3d5f17ee509f6f17d1383a538db1345dd6968f
Jora <jora_a@outlook.com>
2023-07-14 17:14:28 +0330

    fix: remove useState hook from useQuery

---
+++ hooks/useQuery.ts
@@ -0,0 +1,30 @@
+import { useQuery as _useQuery } from '@tanstack/react-query';
+
+type QueryOptions = {
+  timeout: number;
+  ContentType?: string
+}
+
+function useQuery<T>(url: string, token: string, options: QueryOptions) {
+  const { data, isSuccess, isError, isLoading, status } = _useQuery([token], async () => {
+    const controller = new AbortController();
+    const abortId = setTimeout(() => controller.abort(), options.timeout);
+
+    const response = await window.fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': options.ContentType ?? 'application/json'
+      },
+      signal: controller.signal
+    });
+
+    clearTimeout(abortId);
+
+    return response.json() as T;
+  }
+  );
+
+  return { status, data, isSuccess, isError, isLoading };
+}
+
+export default useQuery;
