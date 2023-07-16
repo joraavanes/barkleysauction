1309084e8af07d472cf58f30f65cf428094b9136
Jora <jora_a@outlook.com>
2023-07-16 12:07:25 +0330

    Add Links of all pages to the Navigation

--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -9,9 +9,33 @@ const Navigation: React.FC = ({}) => {
         <Link href={'/items'}>
             Items
         </Link>
+         / 
+        <Link href={'/items/my-items'}>
+            My Items
+        </Link>
+         / 
+        <Link href={'/items/create'}>
+            Create
+        </Link>
         /
         <Link href={'/reviews'}>
             Reviews
+        </Link>
+        /
+        <Link href={'/admin/items'}>
+            Items(Admin)
+        </Link>
+        /
+        <Link href={'/admin/users'}>
+            User(Admin)
+        </Link>
+        /
+        <Link href={'/users/login'}>
+            Login
+        </Link>
+        /
+        <Link href={'/users/register'}>
+            Register
         </Link>
     </div>
 };
