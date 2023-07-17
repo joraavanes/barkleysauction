45ca16f358473e25562c7cbc41cc9d82c2734e59
Jora <jora_a@outlook.com>
2023-07-17 14:52:30 +0330

    rm /api/items/create path

--- pages/api/items/create.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { routeMapper } from "../../../src/modules/items";
-
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-    routeMapper(req, res);
-}
-
-export default handler;
