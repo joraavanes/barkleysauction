7c47e5bd180d4ddae918fec5980a7580b6ab28f8
Jora <jora_a@outlook.com>
2023-07-22 18:07:36 +0330

    fix: filter out invalid values for imageUrl

--- src/modules/items/dtos/createItem.dto.ts
+++ src/modules/items/dtos/createItem.dto.ts
@@ -1,13 +1,14 @@
-import { Type } from "class-transformer";
+import { Transform, Type } from "class-transformer";
 
 export class CreateItem {
   title: string;
-  
+
   description: string;
 
   @Type(() => Number)
   startingBid: number;
 
+  @Transform(({ value }) => value && value !== 'null' ? value : null)
   imageUrl: string;
 
   UserId: string;
