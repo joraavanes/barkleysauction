bedeecbe2f7a619b9edfeec423f9a8fb96663e12
Jora <jora_a@outlook.com>
2023-07-25 12:00:20 +0330

    Add bootstrap to the root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
-import "../styles/globals.css";
 import "reflect-metadata";
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/globals.css";
 import { NextPage } from "next";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import NextProgress from "nextjs-progressbar";
-import { ReactElement, ReactNode } from "react";
-import MainLayout from "../components/layout/Layout";
-import { ErrorBoundary } from "../components/shared/";
+import { ReactElement, ReactNode, useEffect } from "react";
+import MainLayout from "@/components/layout/Layout";
+import { ErrorBoundary } from "@/components/shared/";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -22,10 +23,14 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout =
     Component.getLayout ?? ((page) => <MainLayout>{page}</MainLayout>);
 
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return getLayout(
     <ErrorBoundary>
       <QueryClientProvider client={queryClient}>
-        <NextProgress color="#F91890" />
+        <NextProgress color="#101e8e" />
         <Component {...pageProps} />
       </QueryClientProvider>
     </ErrorBoundary>
