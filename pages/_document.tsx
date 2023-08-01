f2d5f5ddafcfc47c2bd5ec681ee0cffb3541fc21
Jora <jora_a@outlook.com>
2023-08-01 17:06:54 +0330

    Move general meta tags to _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta name="theme-color" content="#101e8e" />
+        <link rel="icon" href="/icon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
