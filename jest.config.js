1b2263888afe656e0ef864f0f3d9f318408b128b
Jora <jora_a@outlook.com>
2023-08-19 11:31:05 +0330

    build: Add module aliases to jest config

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,8 @@ const customJestConfig = {
   moduleNameMapper: {
     "@/components/(.*)$": "<rootDir>/components/$1",
     "@/pages/(.*)$": "<rootDir>/pages/$1",
+    "@/shared/(.*)$": "<rootDir>/shared/$1",
+    "@/hooks/(.*)$": "<rootDir>/hooks/$1",
   },
   testEnvironment: "jest-environment-jsdom",
 };
