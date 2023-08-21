e44af4b56c89d5e2e7d3a6a0470782b3d278916b
Jora <jora_a@outlook.com>
2023-08-21 17:49:11 +0330

    fix: return routeMapper

--- pages/api/bids/[id].ts
+++ pages/api/bids/[id].ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { routeMapper } from '../../../src/modules/bids';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  routeMapper(req, res);
+  return routeMapper(req, res);
 }
 
 export default handler;
