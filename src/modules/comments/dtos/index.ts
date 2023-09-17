facf2c7d1a85fe9f6e9760a10c577bc1dab4e962
Jora <jora_a@outlook.com>
2023-09-17 13:25:30 +0330

    rename: change editCommentdto to updateCommentDto

--- src/modules/comments/dtos/index.ts
+++ src/modules/comments/dtos/index.ts
@@ -1,4 +1,4 @@
 export * from './createComment.dto';
-export * from './editComment.dto';
+export * from './updateComment.dto';
 export * from './deleteComment.dto';
 export * from './findComments.dto';
---
+++ src/modules/comments/dtos/updateComment.dto.ts
@@ -0,0 +1,17 @@
+import { Transform } from 'class-transformer';
+import { IsString } from 'class-validator';
+import { ObjectId } from 'mongodb';
+
+export class UpdateCommentDto {
+  @Transform(({ value }) => new ObjectId(value))
+  _id: ObjectId;
+
+  @IsString()
+  content: string;
+
+  @Transform(({ value }) => new ObjectId(value))
+  item: ObjectId;
+
+  @Transform(({ value }) => new ObjectId(value))
+  user: ObjectId;
+}
