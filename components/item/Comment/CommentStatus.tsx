62fea10ef4991073f6c85443048e68f42a432f3f
Jora <jora_a@outlook.com>
2023-09-23 16:05:52 +0330

    Add styled spinner to CommentStatus while pending

--- components/item/Comment/CommentStatus.tsx
+++ components/item/Comment/CommentStatus.tsx
@@ -4,8 +4,14 @@ export const CommentStatus: React.FC = () => {
   const [state] = useComment();
   const { status, error } = state;
 
-  if (status === Status.pending) return <>loading</>;
+  if (status === Status.pending)
+    return (
+      <div className="spinner-grow text-primary" role="status">
+        <span className="sr-only" role="loading"></span>
+      </div>
+    );
+
   if (status === Status.error) return <div role="alert">{error}</div>;
 
-  return <>{Status[status]}</>;
+  return null;
 };
