9e02978f030d608520f4e0c03cef2a09c1ea5116
Jora <jora_a@outlook.com>
2023-09-28 15:02:04 +0330

    feat: apply useMutation to handle comment posting

--- components/item/Comment/CommentForm.tsx
+++ components/item/Comment/CommentForm.tsx
@@ -1,10 +1,28 @@
-import React, { FormEvent, useState } from "react";
-import { postComment } from "../getComments";
+import React, { FormEvent, useEffect, useState } from "react";
 import { Status, useComment } from "./Comment";
+import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "@/pages/_app";
 
 export const CommentForm: React.FC = () => {
-  const [{ status }, dispatch] = useComment();
+  const [{ status, itemId }, dispatch] = useComment();
   const [comment, setComment] = useState("");
+
+  const {
+    error,
+    mutate,
+    status: mutationStatus,
+  } = useMutation({
+    mutationFn: async (body: any) => {
+      const response = await fetch("/api/comments", {
+        body: JSON.stringify(body),
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return response.json();
+    },
+  });
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
@@ -18,21 +36,31 @@ export const CommentForm: React.FC = () => {
     }
 
     dispatch({ type: Status.pending });
-    postComment(comment).then(
-      (comments) => {
-        dispatch({ type: Status.success, comments });
-        setComment("");
-      },
-      (error) => dispatch({ type: Status.error, error })
-    );
+    mutate({
+      user: "63510298c6f3a606c53f8e69",
+      item: itemId,
+      content: comment,
+    });
   };
+
+  useEffect(() => {
+    if (mutationStatus === "success") {
+      dispatch({ type: Status.success });
+      setComment("");
+      queryClient.invalidateQueries({ queryKey: [`comments/${itemId}`] });
+    }
+
+    if (mutationStatus === "error") {
+      dispatch({ type: Status.error, error: error.message });
+    }
+  }, [mutationStatus]);
 
   return (
     <div className="row">
       <div className="col-12 col-lg-8 mt-3 mb-4">
         <form onSubmit={handleSubmit}>
           <div className="row gx-1 gy-1">
-            <div className="col-12 col-lg-11">
+            <div className="col-12 col-lg-10">
               <textarea
                 className="form-control"
                 value={comment}
@@ -41,11 +69,19 @@ export const CommentForm: React.FC = () => {
                 placeholder="Post your comment here ..."
               ></textarea>
             </div>
-            <div className="col-12 col-lg-1">
+            <div className="col-12 col-lg-2">
               <button
                 className="btn btn-primary"
                 disabled={status === Status.pending || !comment.length}
               >
+                {mutationStatus === "loading" ? (
+                  <div
+                    className="spinner-border spinner-border-sm"
+                    role="status"
+                  >
+                    <span className="sr-only"></span>
+                  </div>
+                ) : null}{" "}
                 Post
               </button>
             </div>
