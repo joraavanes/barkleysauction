0f58dc6018bda8aef76a987eb45012b7d98df5df
Jora <jora_a@outlook.com>
2023-10-04 17:44:10 +0330

    feat: Implement UI of Login/Register pages

--- pages/users/login.tsx
+++ pages/users/login.tsx
@@ -1,5 +1,8 @@
 import { randomBytes } from "crypto";
 import { NextPage } from "next";
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
 import { ChangeEvent, FormEvent, useState } from "react";
 
 const sleep = (timeout: number) =>
@@ -22,7 +25,7 @@ const UserLoginPage: NextPage = () => {
 
   const { email, password } = credentials;
 
-  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleLoginSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     loginUser(email, password);
@@ -36,37 +39,80 @@ const UserLoginPage: NextPage = () => {
 
   return (
     <>
-      <h2>Login</h2>
-      <div>
-        <form onSubmit={handleFormSubmit}>
-          <div>
-            <label htmlFor="email">
-              Email
-              <input
-                type="email"
-                name="email"
-                id="email"
-                value={credentials.email}
-                onChange={handleInputChange}
-              />
-            </label>
+      <Head>
+        <title>{`Login | Barkley's Auction`}</title>
+      </Head>
+      <div className="container-fluid">
+        <div className="row mt-4">
+          <div className="col-12 col-sm-10 offset-sm-1">
+            <h2>Login</h2>
+            <hr />
+            <div className="row">
+              <div className="col-12 col-md-6 order-md-2">
+                <div className="row">
+                  <div className="col-10 offset-1 mt-1">
+                    <h2 className="text-center">
+                      Welcome to Barkley&apos;s Auction
+                    </h2>
+                    <Image
+                      src={"/brand-1.png"}
+                      width={1200}
+                      height={1200}
+                      priority={true}
+                      alt="Barkley's Auction"
+                    />
+                  </div>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, quas eligendi per ut, in pri
+                  epicuri probatus. Te vel vocibus placerat scripserit, sit stet
+                  qualisque adversarium in.
+                </p>
+              </div>
+
+              <div className="col-12 col-md-6 order-md-1 mt-3">
+                <p>
+                  You can login through the form below. You can also login
+                  through your facebook or twitter account.
+                </p>
+                <form onSubmit={handleLoginSubmit}>
+                  <p>
+                    <input
+                      type="email"
+                      name="email"
+                      className="form-control"
+                      placeholder="Username or Email"
+                      value={credentials.email}
+                      onChange={handleInputChange}
+                    />
+                  </p>
+                  <p>
+                    <input
+                      type="password"
+                      name="password"
+                      className="form-control"
+                      placeholder="Password"
+                      value={credentials.password}
+                      onChange={handleInputChange}
+                    />
+                  </p>
+                  <p>
+                    <button type="submit" className="btn btn-primary">
+                      Login
+                    </button>
+                  </p>
+                  <h1 className="text-center">Or</h1>
+
+                  <Link href="/users/register" style={{ width: "80%" }}>
+                    <a href="" className="btn btn-primary d-block mx-auto">
+                      You can also register here :)
+                    </a>
+                  </Link>
+                </form>
+              </div>
+            </div>
           </div>
-          <div>
-            <label htmlFor="password">
-              Password
-              <input
-                type="password"
-                name="password"
-                id="password"
-                value={credentials.password}
-                onChange={handleInputChange}
-              />
-            </label>
-          </div>
-          <div>
-            <button type="submit">Login</button>
-          </div>
-        </form>
+        </div>
       </div>
     </>
   );
--- pages/users/register.tsx
+++ pages/users/register.tsx
@@ -1,7 +1,10 @@
 import { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import { ChangeEvent, FormEvent, useState } from "react";
 
-type UserInfo = {
+type RegisterData = {
   email: string;
   password: string;
   firstname: string;
@@ -9,7 +12,7 @@ type UserInfo = {
 };
 
 const UserRegisterPage: NextPage = () => {
-  const [userInfo, setUserInfo] = useState<UserInfo>({
+  const [registerData, setRegisterData] = useState<RegisterData>({
     email: "",
     password: "",
     firstname: "",
@@ -20,74 +23,109 @@ const UserRegisterPage: NextPage = () => {
     e.preventDefault();
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) =>
-    setUserInfo((prev) => ({
+    setRegisterData((prev) => ({
       ...prev,
       [e.target.name]: e.target.value,
     }));
 
   return (
-    <div>
-      <h1>Register</h1>
-      <div>
-        <form onSubmit={handleRegisterSubmit}>
-          <div>
-            <label htmlFor="email">
-              Email
-              <input
-                type="email"
-                name="email"
-                id="email"
-                autoComplete="off"
-                value={userInfo.email}
-                onChange={handleInputChange}
-              />
-            </label>
+    <>
+      <Head>
+        <title>{`Register | Barkley's Auction`}</title>
+      </Head>
+      <div className="container-fluid">
+        <div className="row mt-4">
+          <div className="col-12 col-sm-10 offset-sm-1">
+            <h2>Register</h2>
+            <hr />
+            <div className="row">
+              <div className="col-12 col-md-6 order-md-2">
+                <div className="row">
+                  <div className="col-10 offset-1">
+                    <h2 className="text-center">
+                      Welcome to Barkley&apos;s Auction
+                    </h2>
+                    <Image
+                      src={"/brand-1.png"}
+                      width={1200}
+                      height={1200}
+                      priority={true}
+                      alt="Barkley's Auction"
+                    />
+                  </div>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, quas eligendi per ut, in pri
+                  epicuri probatus. Te vel vocibus placerat scripserit, sit stet
+                  qualisque adversarium in.
+                </p>
+              </div>
+
+              <div className="col-12 col-md-6 order-md-1 mt-3">
+                <p>
+                  You can register through the form below. You can also login
+                  through your facebook or twitter account.
+                </p>
+                <form onSubmit={handleRegisterSubmit}>
+                  <p>
+                    <input
+                      type="email"
+                      name="email"
+                      className="form-control"
+                      placeholder="Username or Email"
+                      value={registerData.email}
+                      onChange={handleInputChange}
+                    />
+                  </p>
+                  <p>
+                    <input
+                      type="password"
+                      name="password"
+                      className="form-control"
+                      placeholder="Password"
+                      value={registerData.password}
+                      onChange={handleInputChange}
+                    />
+                  </p>
+                  <p>
+                    <input
+                      type="text"
+                      name="firstname"
+                      className="form-control"
+                      placeholder="Firstname"
+                      value={registerData.firstname}
+                      onChange={handleInputChange}
+                    />
+                  </p>
+                  <p>
+                    <input
+                      type="text"
+                      name="lastname"
+                      className="form-control"
+                      placeholder="Lastname"
+                      value={registerData.lastname}
+                      onChange={handleInputChange}
+                    />
+                  </p>
+                  <p>
+                    <button type="submit" className="btn btn-primary">
+                      Login
+                    </button>
+                  </p>
+                  <h1 className="text-center">Or</h1>
+
+                  <Link href="/users/login" style={{ width: "80%" }} passHref>
+                    <a href="" className="btn btn-primary d-block mx-auto">
+                      You can also log in here :)
+                    </a>
+                  </Link>
+                </form>
+              </div>
+            </div>
           </div>
-          <div>
-            <label htmlFor="password">
-              Password
-              <input
-                type="password"
-                name="password"
-                id="password"
-                autoComplete="off"
-                value={userInfo.password}
-                onChange={handleInputChange}
-              />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="firstname">
-              Firstname
-              <input
-                type="text"
-                name="firstname"
-                id="firstname"
-                autoComplete="off"
-                value={userInfo.firstname}
-                onChange={handleInputChange}
-              />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="lastname">
-              Lastname
-              <input
-                type="text"
-                name="lastname"
-                id="lastname"
-                autoComplete="off"
-                value={userInfo.lastname}
-                onChange={handleInputChange}
-              />
-            </label>
-          </div>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-        </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
