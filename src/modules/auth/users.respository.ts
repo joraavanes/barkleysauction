14978b83f5ab4d86406a90e3c15ae43d8da7275c
Jora <jora_a@outlook.com>
2023-10-16 17:18:02 +0330

    feat: add signUp logic, inherit usersRepo from BaseRepo

--- src/modules/auth/users.respository.ts
+++ src/modules/auth/users.respository.ts
@@ -1,4 +1,3 @@
-import { ObjectId, WithId } from "mongodb";
 import { Service } from "typedi";
 import { MongoDbClient } from "../../db";
 import { User } from "./models/user.model";
@@ -8,31 +7,7 @@ import { BaseRepository } from "@/src/db/Repository";
 export class UsersRepository extends BaseRepository<User> {
   constructor(
     private mongo: MongoDbClient
-  ) { 
+  ) {
     super('users');
-  }
-
-  async createOne(model: User) {
-    return (await this.mongo.getClient())
-      .db()
-      .collection('users')
-      .insertOne(model);
-  }
-
-  async findById(id: string) {
-    return (await this.mongo.getClient())
-      .db()
-      .collection('users')
-      .findOne({
-        _id: new ObjectId(id)
-      });
-  }
-
-  async findAll(): Promise<WithId<User>[]> {
-    return await (await this.mongo.getClient())
-      .db()
-      .collection<User>('users')
-      .find()
-      .toArray();
   }
 }
--- src/modules/auth/users.service.ts
+++ src/modules/auth/users.service.ts
@@ -1,7 +1,11 @@
+import { scrypt as _scrypt, randomBytes } from 'node:crypto';
 import { Service } from "typedi";
 import { User } from "./models/user.model";
 import { UsersRepository } from "./users.respository";
 import { ObjectId } from "mongodb";
+import { promisify } from 'node:util';
+
+const scrypt = promisify(_scrypt);
 
 @Service()
 export class UsersService {
@@ -13,11 +17,31 @@ export class UsersService {
     return this.usersRepository.findOne({ _id: new ObjectId(id) })
   }
 
+  findByEmail(email: string) {
+    return this.usersRepository.findOne({ email });
+  }
+
   async getAll() {
-    return this.usersRepository.findAll();
+    return this.usersRepository.find({ limit: 100, offset: 0 });
   }
 
   async createUser(model: User) {
-    return this.usersRepository.createOne(model);
+    const existingUser = await this.findByEmail(model.email);
+
+    if (existingUser) {
+      throw new Error('User with the email already exists');
+    }
+
+    const salt = randomBytes(16).toString('hex');
+
+    const hash = await scrypt(model.password, salt, 32) as Buffer;
+    const hashWithSalt = `${hash.toString('hex')}.${salt}`;
+
+    const user: User = {
+      ...model,
+      password: hashWithSalt
+    };
+
+    return this.usersRepository.create(user);
   }
 }
