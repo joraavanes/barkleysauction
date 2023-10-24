75f7ff38ff41ef14c06921d1634dda5118bb9d51
Jora <jora_a@outlook.com>
2023-10-24 14:35:22 +0330

    Add server-side guard to /create component

--- pages/items/create.tsx
+++ pages/items/create.tsx
@@ -1,11 +1,27 @@
-import { ReactElement } from "react";
+import { useEffect } from "react";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
-import CreateItem from "@/components/item/CreateItem";
-import { Item } from "@/components/item/Item";
+import { useRouter } from "next/router";
+import { getSession, useSession } from "next-auth/react";
 import { NextPageWithLayout } from "../_app";
+import CreateItem from "@/components/item/CreateItem";
 import ItemStatus from "@/components/item/ItemStatus";
+import { Item } from "@/components/item/Item";
 
 const CreateItemPage: NextPageWithLayout = () => {
+  const router = useRouter();
+  const { data, status } = useSession();
+
+  useEffect(() => {
+    if (!data && status === "unauthenticated") {
+      router.push("/users/login");
+    }
+  }, [data, status]);
+
+  if (status === "loading") {
+    return <div>Loading ...</div>;
+  }
+
   return (
     <>
       <Head>
@@ -24,6 +40,24 @@ const CreateItemPage: NextPageWithLayout = () => {
       </div>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      props: {},
+      redirect: {
+        destination: "/users/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 // CreateItemPage.getLayout = function getLayout(page: ReactElement) {
