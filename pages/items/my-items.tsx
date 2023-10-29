fe25743567453a1dd9eedccd1ceb43d0918bdefd
Jora <jora_a@outlook.com>
2023-10-29 11:18:07 +0330

    Add authentication to /items/my-items route

--- pages/items/my-items.tsx
+++ pages/items/my-items.tsx
@@ -4,8 +4,23 @@ import { itemsService } from "@/src/modules/items";
 import { Item as ItemModel } from "@/src/modules/items/item.model";
 import { Item } from "@/components/item/Item";
 import MyItems from "@/components/item/my-items";
+import { getSession, useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/users/login",
+      },
+      props: {},
+    };
+  }
+
   const items = await itemsService.getItems({
     limit: Number.MAX_SAFE_INTEGER,
     offset: 0,
@@ -23,6 +38,19 @@ export const getServerSideProps: GetServerSideProps = async () => {
 };
 
 const MyItemsPage: NextPageWithLayout<{ items: ItemModel[] }> = ({ items }) => {
+  const router = useRouter();
+  const { data, status } = useSession();
+
+  useEffect(() => {
+    if (!data && status === "unauthenticated") {
+      router.push("/users/login");
+    }
+  }, [data, status]);
+
+  if (status === "loading") {
+    return <div>Loading ...</div>;
+  }
+
   return (
     <>
       <Item>
