b3b5e8626ce451b9565b4e39599d31ea7e93d37c
Jora <jora_a@outlook.com>
2023-10-30 18:08:47 +0330

    fix: add firstname/lastname prop to User domain model

--- src/modules/auth/models/user.model.ts
+++ src/modules/auth/models/user.model.ts
@@ -1,12 +1,11 @@
 import { ObjectId } from "mongodb";
-import { Item } from "../../items/item.model";
 
 export interface User {
     _id: ObjectId,
     email: string;
     password: string;
     userConfirmed: boolean;
-    name: string;
-    surname: string;
+    firstname: string;
+    lastname: string;
     items?: Array<ObjectId>;
 }
