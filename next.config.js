02939ec393e51dfd3d1e3285247c2c7cfa68d2ba
Jora <jora_a@outlook.com>
2023-11-05 19:36:56 +0330

    Add remote resource for data on cloud

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   env: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mystifying-khayyam-9evvnoqws.storage.iran.liara.space",
+        port: "",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
