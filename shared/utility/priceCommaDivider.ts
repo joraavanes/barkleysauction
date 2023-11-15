727c451d7d7e662d7e53018d454b79d682927ac8
Jora <jora_a@outlook.com>
2023-11-15 12:41:23 +0330

    add comma divider function for prices

---
+++ shared/utility/priceCommaDivider.ts
@@ -0,0 +1,14 @@
+
+export default function (value: number | string) {
+  var nStr = value + '';
+  nStr = nStr.replace(/\,/g, "");
+  let x = nStr.split('.');
+  let x1 = x[0];
+  let x2 = x.length > 1 ? '.' + x[1] : '';
+  var rgx = /(\d+)(\d{3})/;
+  while (rgx.test(x1)) {
+    x1 = x1.replace(rgx, '$1' + ',' + '$2');
+  }
+  return x1 + x2;
+}
+
